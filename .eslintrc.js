050514bfb93c9af34dec7c1a2a85af9c04f8fc2e
heinhoang <bachdaonhan@gmail.com>
2018-03-03 09:49:41 +0700

    adjust eslint & travis

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,19 @@
 module.exports = {
+    "env": {
+        "es6": true,
+        "browser": true,
+        "mocha": true,
+        "node": true,
+        "phantomjs": true,
+        "protractor": true
+    },
     "extends": "airbnb",
     "rules": {
-        "import/no-unresolved": [2, {"commonjs": true, "amd": true}],
+        "import/no-unresolved": [2, { "commonjs": true, "amd": true }],
         "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
     },
+    "globals": {
+        "navigator": true,
+        "fetch": true,
+    }
 };
